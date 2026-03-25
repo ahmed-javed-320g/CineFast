40a0a84e7797c9d4914d3ba8a88f523e7ceae241
ahmed-javed-320g <ahmed.javed.320g@gmail.com>
2026-03-25 19:40:35 +0500

    created MovieAdapter using recycler view and also created a NowShowing fragment

---
+++ app/src/main/java/com/example/a0644_a1/MovieAdapter.java
@@ -0,0 +1,68 @@
+package com.example.a0644_a1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    public interface OnBookClickListener {
+        void onBookClick(Movie movie);
+    }
+
+    private ArrayList<Movie> movies;
+    private OnBookClickListener listener;
+
+    public MovieAdapter(ArrayList<Movie> movies, OnBookClickListener listener) {
+        this.movies = movies;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+        return new MovieViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvGenre.setText(movie.getGenre());
+        holder.ivPoster.setImageResource(movie.getPosterResId());
+
+        holder.btnTrailer.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(movie.getTrailerUrl()));
+            v.getContext().startActivity(intent);
+        });
+
+        holder.btnBook.setOnClickListener(v -> listener.onBookClick(movie));
+    }
+
+    @Override
+    public int getItemCount() { return movies.size(); }
+
+    static class MovieViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivPoster;
+        TextView tvTitle, tvGenre;
+        Button btnBook, btnTrailer;
+
+        MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivPoster = itemView.findViewById(R.id.IVmovie);
+            tvTitle = itemView.findViewById(R.id.TVmovieTitle);
+            tvGenre = itemView.findViewById(R.id.TVmovieGenre);
+            btnBook = itemView.findViewById(R.id.btnBook);
+            btnTrailer = itemView.findViewById(R.id.btnTrailer);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a0644_a1/NowShowingFragment.java
@@ -0,0 +1,46 @@
+package com.example.a0644_a1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class NowShowingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("The Dark Knight", "Action / 152 min", R.drawable.batman,
+                "https://www.youtube.com/watch?v=EXeTwQWrcwY", false));
+        movies.add(new Movie("Inception", "Sci-Fi / 148 min", R.drawable.inception,
+                "https://www.youtube.com/watch?v=YoHD9XEInc0", false));
+        movies.add(new Movie("Interstellar", "Sci-Fi / 169 min", R.drawable.interstellar,
+                "https://www.youtube.com/watch?v=zSWdZVtXT7E", false));
+        movies.add(new Movie("The Shawshank Redemption", "Drama / 142 min",
+                R.drawable.the_shawshank_redemption,
+                "https://www.youtube.com/watch?v=PLl99DlL6b4", false));
+
+        RecyclerView rv = view.findViewById(R.id.recyclerViewMovies);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setAdapter(new MovieAdapter(movies, movie -> {
+            MainActivity activity = (MainActivity) requireActivity();
+            activity.setSelectedMovie(movie.getTitle(), movie.getPosterResId(), false);
+            activity.loadFragment(new SeatSelectionFragment(), true);
+        }));
+    }
+}
