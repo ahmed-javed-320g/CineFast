0a3a233eb0677cd4d1d1b71ef8861eaf2d37e143
ahmed-javed-320g <ahmed.javed.320g@gmail.com>
2026-04-26 00:12:19 +0500

    added firebase and other dependencies to app level and project level gradle build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services") apply false
 }
 
 android {
@@ -43,6 +44,14 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
+
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
+    //implementation("com.google.android.material:material:1.9.0")
 }
